252defd1627aed2092b8d0a561b4d02f90d3eece
Nihal Ramaswamy <nihalramaswamy@gmail.com>
2024-12-01 11:43:03 +0530

    Updated tmux theme

--- bootstrap_mac.sh
+++ bootstrap_mac.sh
@@ -11,19 +11,19 @@ fi
 curl -fLo $HOME/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # Tmux tpm
-git clone https://github.com/tmux-plugins/tpm ~/.local/.tmux/plugins/tpm
+git clone https://github.com/tmux-plugins/tpm ~/.local/share/.tmux/plugins/tpm
 
 
 # Dotfile manager
 brew install stow
-stow -vSt ~ brew git vim zsh antidote
+stow -vSt ~ brew git vim zsh antidote tmux
 
 brew bundle --file $HOME/.config/brew/.Brewfile
 
 source ~/.zshrc
 
 # Antidote
-git clone --depth=1 https://github.com/mattmc3/antidote.git ${XDG_DATA_HOME}/.antidote
+git clone --depth=1 https://github.com/mattmc3/antidote.git ~/.local/share/.antidote
 
 # sdkman
 curl -s "https://get.sdkman.io" | bash
