90e52a31f740d2ecc3ca77ca9bd3dba21cabd3a4
Nihal Ramaswamy <nihalramaswamy@gmail.com>
2025-08-30 06:58:20 +0530

    Update cp folder

--- cp/.config/cp/cp.sh
+++ cp/.config/cp/cp.sh
@@ -7,7 +7,23 @@ function cpt() {
 	done
 }
 
+function cpr() {
+	fileName=$1
+	g++ -Wall -Wextra -Wconversion -DONLINE_JUDGE -Wl,-stack_size,0x10000000 -O2 -std=c++20 $fileName -o ${fileName%.cpp}
+}
+
 function cpb() {
 	fileName=$1
 	g++ -std=c++20 -DDEBUG -O2 -Wall -Wshadow -Wextra -Wno-unused-result -Wfloat-equal -Wcast-qual -Wcast-align -Wl,-stack_size,0x10000000 -g -fstack-protector -D_GLIBCXX_DEBUG $fileName -o ${fileName%.cpp}
+}
+
+function stress() {
+	for ((i = 1; ; i++)); do 
+		fileName=$1
+		echo $i 
+		python3 stress.py > in1
+		./$fileName < in1 > out1 
+		./brute < in1 > out2 
+		diff -w out1 out2 || break 
+	done
 }
